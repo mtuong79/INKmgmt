21c0b7d00582bc5d529d62687135e57fdffe9b27
mtuong79 <89337936+mtuong79@users.noreply.github.com>
2022-06-11 12:52:43 +0700

    Update DataApp.py
    Add dept BioStats

--- DataApp.py
+++ DataApp.py
@@ -1,5 +1,3 @@
-# MT@OUCRU
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,6 +6,7 @@ from datetime import date
 import base64
 import smtplib, ssl
 from email.message import EmailMessage
+
 
 def Email_Order(msgbody):
     # Less secure app access on Gmail Security: Turn on
@@ -21,9 +20,9 @@ def Email_Order(msgbody):
     message = f"""\
     Hi !
     Please order the Ink Cartrdiges with the code and quantity as below,
-    
+
     {msgbody}
-    
+
     Thanks,
     ServiceDesk"""
 
@@ -39,21 +38,26 @@ def Email_Order(msgbody):
         server.starttls(context=context)
         server.ehlo()  # Can be omitted
         server.login(sender_email, password)
-        #server.sendmail(sender_email, receiver_email, message)
+        # server.sendmail(sender_email, receiver_email, message)
         server.send_message(msg)
 
     st.success(f"Your order is conformed and sent to manhtuong@gmail.com")
 
+
 def Xuat_Kho(df):
     file = "Data/Data_InkMgmt.csv"
     st.subheader("STOCK EXPORT")
-    #st.write(df)
-    #st.dataframe(df.style.highlight_min(axis=0))
+    # st.write(df)
+    # st.dataframe(df.style.highlight_min(axis=0))
     df['Inventory'] = df['Inventory'].astype(int)
     df['Admin'] = df['Admin'].astype(int)
     df['Account'] = df['Account'].astype(int)
     df['CTU'] = df['CTU'].astype(int)
     df['EI'] = df['EI'].astype(int)
+    df['IT'] = df['IT'].astype(int)
+    df['Estate'] = df['Estate'].astype(int)
+    df['MolEPI'] = df['MolEPI'].astype(int)
+    df['BioStats'] = df['BioStats'].astype(int)
     df['Modelling'] = df['Modelling'].astype(int)
     df['PE'] = df['PE'].astype(int)
     df['Malaria'] = df['Malaria'].astype(int)
@@ -67,13 +71,15 @@ def Xuat_Kho(df):
     df['InkTotal'] = df['InkTotal'].astype(int)
 
     with st.form("my_form"):
-        department = st.selectbox("Department", ('Admin', 'Account', 'CTU', 'EI','IT', 'Estate', 'Modelling', 'PE', 'Malaria', 'CNS', 'Dengue', 'TB', 'Lab', 'MicroLab', 'Zoonoses', 'VA-ward'))
-        inkcode = st.selectbox("Ink Code",df['InkCode'])
-        #x_quantity = st.number_input(f"Quantity", value=0)
+        department = st.selectbox("Department", (
+        'Admin', 'Account', 'CTU', 'EI', 'IT', 'Estate','MolEPI','BioStats', 'Modelling', 'PE', 'Malaria', 'CNS', 'Dengue', 'TB', 'Lab',
+        'MicroLab', 'Zoonoses', 'VA-ward'))
+        inkcode = st.selectbox("Ink Code", df['InkCode'])
+        # x_quantity = st.number_input(f"Quantity", value=0)
 
         submitted = st.form_submit_button("Submit")
         if submitted:
-            Thang =  date.today().strftime("%b")
+            Thang = date.today().strftime("%b")
 
             inventory = df.loc[inkcode, 'Inventory']
 
@@ -89,14 +95,15 @@ def Xuat_Kho(df):
 
     df.to_csv(file, index=True)
 
+
 def Nhap_Kho(df):
     file = "Data/Data_InkMgmt.csv"
     st.subheader("STOCK IMPORT")
-    #st.dataframe(df[['Printer','InkCode','Inventory','IT-Warehouse']])
+    # st.dataframe(df[['Printer','InkCode','Inventory','IT-Warehouse']])
     df['Inventory'] = df['Inventory'].astype(int)
 
     with st.form("my_form"):
-        inkcode = st.selectbox("Ink Code",df['InkCode'])
+        inkcode = st.selectbox("Ink Code", df['InkCode'])
         n_quantity = st.number_input("Quantity", value=0)
         submitted = st.form_submit_button("Submit")
         if submitted:
@@ -105,29 +112,30 @@ def Nhap_Kho(df):
             df.loc[inkcode, 'Inventory'] = inventory + n_quantity
             df.loc[inkcode, 'InkTotal'] = total + n_quantity
 
-            #st.write(f"Successfully Updated Inventory: {inkcode} , Totlal: {df.loc[inkcode, 'Inventory']}")
+            # st.write(f"Successfully Updated Inventory: {inkcode} , Totlal: {df.loc[inkcode, 'Inventory']}")
             st.success(f"Successfully Imported: {inkcode} , Total in Stock: {df.loc[inkcode, 'Inventory']}")
 
-        st.write(df[['Printer','InkCode','Inventory','IT-Warehouse']])
+        st.write(df[['Printer', 'InkCode', 'Inventory', 'IT-Warehouse']])
 
     df.to_csv(file, index=True)
+
 
 def Thong_Ke(df):
     file = "Data/Data_InkMgmt.csv"
     st.subheader("STOCK REPORT")
-    tk = st.radio("",('Inventory Summary', 'Usage by Department', 'Total of Ink Cartridges','Order of Ink'))
+    tk = st.radio("", ('Inventory Summary', 'Usage by Department', 'Total of Ink Cartridges', 'Order of Ink'))
 
     if tk == 'Inventory Summary':
         df.sort_values(by='Inventory', ascending=False)
-        #st.bar_chart(df['Inventory'])
+        # st.bar_chart(df['Inventory'])
         chart = (
             alt.Chart(df).mark_bar().encode(
                 x='Inventory',
                 y=alt.Y('InkCode', sort='-x')
             )
-                #.properties(width=800)
+            # .properties(width=800)
         )
-        #text = chart.mark_text(align="left", baseline="middle", dx=3).encode(text="Number of data")
+        # text = chart.mark_text(align="left", baseline="middle", dx=3).encode(text="Number of data")
         st.altair_chart(chart)
 
         st.subheader("Out of Stock")
@@ -139,11 +147,12 @@ def Thong_Ke(df):
 
     elif tk == 'Usage by Department':
         st.write("Usage by Department")
-        data = df[['Admin','Account','CTU','EI','IT','Modelling','PE','Malaria','CNS','Dengue','TB','Lab','MicroLab','Zoonoses','VA-ward']]
+        data = df[['Admin', 'Account', 'CTU', 'EI', 'IT','Estate','MolEPI','BioStats', 'Modelling', 'PE', 'Malaria', 'CNS', 'Dengue', 'TB', 'Lab',
+                   'MicroLab', 'Zoonoses', 'VA-ward']]
         st.bar_chart(data)
     elif tk == 'Total of Ink Cartridges':
         st.subheader("Total of Ink Cartridges")
-        tdata = df[['Printer','InkCode','InkTotal']].sort_values(by='InkTotal', ascending=False)
+        tdata = df[['Printer', 'InkCode', 'InkTotal']].sort_values(by='InkTotal', ascending=False)
         idmin = df['InkTotal'].idxmin()
         idmax = df['InkTotal'].idxmax()
 
@@ -152,7 +161,7 @@ def Thong_Ke(df):
         st.bar_chart(tdata['InkTotal'])
 
         st.subheader("Ink-Cartridge Usage by Month")
-        ydata = df[['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']]
+        ydata = df[['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']]
         T1 = df['Jan'].sum()
         T2 = df['Feb'].sum()
         T3 = df['Mar'].sum()
@@ -167,15 +176,15 @@ def Thong_Ke(df):
         T12 = df['Dec'].sum()
 
         source = pd.DataFrame({
-            'Month': ('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'),
-            'TotalInk': (T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12)
+            'Month': ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'),
+            'TotalInk': (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12)
         })
         chart = (
             alt.Chart(source).mark_bar().encode(
-                alt.X("Month", sort=None,title=""),
+                alt.X("Month", sort=None, title=""),
                 y="TotalInk",
-                #column='TotalInk',
-                #color='Month'
+                # column='TotalInk',
+                # color='Month'
             )
                 .properties(width=600)
         )
@@ -192,7 +201,7 @@ def Thong_Ke(df):
     else:
 
         df['OrderQuantity'] = df['OrderQuantity'].astype(int)
-        df['OrderDate'] = pd.to_datetime(df['OrderDate']) # format='%y%m%d'
+        df['OrderDate'] = pd.to_datetime(df['OrderDate'])  # format='%y%m%d'
 
         today = date.today()
         order_date = st.date_input('Order date', today)
@@ -208,27 +217,28 @@ def Thong_Ke(df):
             order_submitted = st.form_submit_button("Order")
 
             if order_submitted:
-                df.loc[sl_order,'OrderQuantity'] = q_order
-                df.loc[sl_order,'OrderDate'] = today
+                df.loc[sl_order, 'OrderQuantity'] = q_order
+                df.loc[sl_order, 'OrderDate'] = today
                 st.success(f"Successfully put the inkcode: {sl_order} and quantity: {q_order} into the order list ")
                 df.to_csv(file, index=True)
 
-        #st.write(f"This is your order of Ink, date: {order_date}")
-        #st.dataframe(df[['InkCode','OrderQuantity']])
+        # st.write(f"This is your order of Ink, date: {order_date}")
+        # st.dataframe(df[['InkCode','OrderQuantity']])
         with st.form("conform_oder"):
             st.write(f"This is your order of Ink, date: {order_date}")
             msgbody = ""
             for inkcode in df['InkCode']:
-                if df.loc[inkcode,'OrderDate'] == today:
-                    st.write(f"{inkcode} : {df.loc[inkcode,'OrderQuantity']}")
+                if df.loc[inkcode, 'OrderDate'] == today:
+                    st.write(f"{inkcode} : {df.loc[inkcode, 'OrderQuantity']}")
                     msgbody += str(inkcode)
                     msgbody += ':'
-                    msgbody += str(df.loc[inkcode,'OrderQuantity'])
+                    msgbody += str(df.loc[inkcode, 'OrderQuantity'])
                     msgbody += '--'
 
             conform_order = st.form_submit_button("Conform Order")
             if conform_order:
                 Email_Order(msgbody)
+
 
 def main():
     st.title("INK CARTRIDGE MANAGEMENT")
@@ -245,8 +255,9 @@ def main():
     if task == 'Stock Export':
         Xuat_Kho(df)
     elif task == 'Stock Import':
-       Nhap_Kho(df)
+        Nhap_Kho(df)
     else:
         Thong_Ke(df)
+
 
 main()
